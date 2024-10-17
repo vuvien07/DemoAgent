1629bb524a0ac191b2de3ea75b940e0f67f29b27
= <96453867+vuvien07@users.noreply.github.com>
2024-10-17 14:55:23 +0700

    removed redundant code

--- DemoAgent/Record.xaml.cs
+++ DemoAgent/Record.xaml.cs
@@ -47,7 +47,6 @@ namespace DemoAgent
         private App app = System.Windows.Application.Current as App;
         private string wavFile = "";
         private string transFile = "";
-        public static string fff;
 
         public Record(Account account, bool isMeeting)
         {
